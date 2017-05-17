3973e284782f9f98cdec692c32bd25b54938b146
Justin Steigel <jsteigel@rmn.com>
2017-05-17 15:23:11 -0500

    added worker component to read from sqs queue of S3 ObjectCreated events and POST to Nudge

--- src/nudge/endpoint.py
+++ src/nudge/endpoint.py
@@ -22,6 +22,10 @@ class Endpoint(Serializable):
     def send_message(self, ctx, msg):
         pass
 
+    @abc.abstractmethod
+    def receive_message(self, ctx, wait_time):
+        pass
+
 
 class SqsEndpoint(Endpoint):
 
@@ -45,4 +49,18 @@ class SqsEndpoint(Endpoint):
         ctx.sqs.send_message(
             QueueUrl=self._queue_url,
             MessageBody=msg,
+        )
+
+    def receive_message(self, ctx, *, max_messages=1, wait_time=20):
+        response = ctx.sqs.receive_message(
+            QueueUrl=self._queue_url,
+            MaxNumberOfMessages=max_messages,
+            WaitTimeSeconds=wait_time
+        )
+        return response.get('Messages')
+
+    def delete_message(self, ctx, receipt_handle):
+        ctx.sqs.delete_message(
+            QueueUrl=self._queue_url,
+            ReceiptHandle=receipt_handle
         )
